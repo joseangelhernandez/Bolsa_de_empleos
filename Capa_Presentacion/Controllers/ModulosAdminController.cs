56927687f8b2c32573bd0868d5ccf9ff02fc186d
Jose Angel Hernandez <joseangel03082010@hotmail.com>
2021-07-01 19:58:39 -0400

    Role y logins listos

---
+++ Capa_Presentacion/Controllers/ModulosAdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.IO;
+
+namespace Capa_Presentacion.Controllers
+{
+    [AccessDeniedAuthorizeAttribute(Roles = "admin")]
+    public class ModulosAdminController : Controller
+    {
+        // GET: ModulosAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Capa_Presentacion/Controllers/ModulosPosterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capa_Presentacion.Controllers
+{
+    [AccessDeniedAuthorizeAttribute(Roles = "poster")]
+    public class ModulosPosterController : Controller
+    {
+        // GET: ModulosPoster
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
